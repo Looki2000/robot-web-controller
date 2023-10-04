69a19625cc9b7d7e9b703035a832e576aafc6ba5
Looki2000 <electro.brzostek@gmail.com>
2023-10-04 14:11:41 +0200

    frame dropping?

--- main.py
+++ main.py
@@ -30,19 +30,38 @@ loop_delay = 1/loop_hz
 #Initialize the Flask app
 app = Flask(__name__)
 
-def gen_frames():
 
+# Create a lock to ensure that frames are sent one at a time
+frame_lock = threading.Lock()
+
+#def gen_frames():
+#
+#    camera = cv2.VideoCapture(0)
+#
+#    while True:
+#        success, frame = camera.read()  # read the camera frame
+#        if not success:
+#            break
+#        else:
+#            #buffer = cv2.imencode('.jpg', frame)[1]
+#            buffer = cv2.imencode('.jpg', frame, encode_param)[1]
+#            frame = buffer.tobytes()
+#            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+def gen_frames():
     camera = cv2.VideoCapture(0)
 
     while True:
-        success, frame = camera.read()  # read the camera frame
+        success, frame = camera.read()
         if not success:
             break
         else:
-            #buffer = cv2.imencode('.jpg', frame)[1]
             buffer = cv2.imencode('.jpg', frame, encode_param)[1]
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+            frame_bytes = buffer.tobytes()
+            
+            with frame_lock:
+                yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+
 
     # stop camera
     camera.release()
@@ -53,9 +72,14 @@ def gen_frames():
 def index():
     return render_template("index.html")
 
+#@app.route('/video_feed')
+#def video_feed():
+#    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 
 
